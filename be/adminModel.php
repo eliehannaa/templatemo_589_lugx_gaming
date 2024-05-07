982b7419e39fa11515f3cf58c6e39df036a206fa
tinobourada <tino.bourada@lau.edu>
2024-05-07 12:54:38 +0300

    backend

--- be/adminModel.php
+++ be/adminModel.php
@@ -8,7 +8,7 @@
 function signup($user, $db)
 {
     //insert query
-    $query = "INSERT INTO `tbl_users` (`USERNAME`,`FIRST_NAME`,`LAST_NAME`,`PASSWORD`) VALUES ('$user->username','$user->firstname','$user->lastname','$user->password')";
+    $query = "INSERT INTO `tbl_users` (`USERNAME`,`PASSWORD`) VALUES ('$user->username','$user->password')";
     $stmt = $db->query($query);
     if ($stmt->rowCount() > 0)
         return 1;
@@ -20,15 +20,14 @@ function GetUsers()
 {
     global $db;
     $users = array();
-    $query = "SELECT ID,USERNAME,FIRST_NAME,LAST_NAME,IS_ACTIVE FROM tbl_users";
+    $query = "SELECT ID,USERNAME FROM tbl_users";
     $stmt = $db->query($query);
     if ($stmt->rowCount() > 0) {
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             $user = new stdClass();
             $user->id = $row["ID"];
             $user->username = $row["USERNAME"];
-            $user->name = $row["FIRST_NAME"] . " " . $row["LAST_NAME"];
-            $user->isActive = $row["IS_ACTIVE"];
+           
             $users[] = $user;
         }
         return $users;
@@ -50,28 +49,17 @@ function DeleteUser($id)
     }
 }
 
-function ActivateUser($id, $isActive)
-{
-    global $db;
-    $users = array();
-    $query = "UPDATE tbl_users SET IS_ACTIVE=$isActive where ID=$id";
-    $stmt = $db->query($query);
-    if ($stmt->rowCount() > 0) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
+
 
 function login($un, $pass)
 {
     global $db;
-    $query = "SELECT ID,FIRST_NAME,LAST_NAME FROM tbl_users WHERE USERNAME='$un' AND PASSWORD='$pass' AND IS_ACTIVE=1";
+    $query = "SELECT ID FROM tbl_users WHERE USERNAME='$un' AND PASSWORD='$pass' AND IS_ACTIVE=1";
     $stmt = $db->query($query);
     if ($stmt->rowCount() > 0) {
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        $name = $row["FIRST_NAME"] . " " . $row["LAST_NAME"];
-        return $name;
+        $user->username = $row["USERNAME"];
+        return $username;
     } else {
         return 0;
     }
