fc8708bb3b8d28befbf1030436461c177b2c755c
tinobourada <tino.bourada@lau.edu>
2024-05-07 13:17:58 +0300

    log out feature

--- be/adminModel.php
+++ be/adminModel.php
@@ -65,4 +65,32 @@ function login($un, $pass)
         return 0;
     }
 }
+
+function LogOut()
+{
+    // Start the session if it hasn't been started yet
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+
+    // Unset all session variables
+    $_SESSION = array();
+
+    // If it's desired to kill the session, also delete the session cookie.
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+
+    // Finally, destroy the session.
+    session_destroy();
+
+    // Redirect to the login page or home page
+    header("Location: index.php");
+    exit;
+}
+
 ?>
